f9b41907e57e0acb716f6876eb55d8ec33248ef1
Dmitrii-ip01 <ip01proexpert@gmail.com>
2019-12-24 14:49:51 +0500

    init

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://retailcrm/admin/');
+define('HTTP_CATALOG', 'http://retailcrm/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://retailcrm/admin/');
+define('HTTPS_CATALOG', 'http://retailcrm/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OSPanel/domains/retailcrm/admin/');
+define('DIR_SYSTEM', 'C:/OSPanel/domains/retailcrm/system/');
+define('DIR_IMAGE', 'C:/OSPanel/domains/retailcrm/image/');
+define('DIR_LANGUAGE', 'C:/OSPanel/domains/retailcrm/admin/language/');
+define('DIR_TEMPLATE', 'C:/OSPanel/domains/retailcrm/admin/view/template/');
+define('DIR_CONFIG', 'C:/OSPanel/domains/retailcrm/system/config/');
+define('DIR_CACHE', 'C:/OSPanel/domains/retailcrm/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OSPanel/domains/retailcrm/system/storage/download/');
+define('DIR_LOGS', 'C:/OSPanel/domains/retailcrm/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OSPanel/domains/retailcrm/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OSPanel/domains/retailcrm/system/storage/upload/');
+define('DIR_CATALOG', 'C:/OSPanel/domains/retailcrm/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'db_retailcrm');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://retailcrm/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://retailcrm/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OSPanel/domains/retailcrm/catalog/');
+define('DIR_SYSTEM', 'C:/OSPanel/domains/retailcrm/system/');
+define('DIR_IMAGE', 'C:/OSPanel/domains/retailcrm/image/');
+define('DIR_LANGUAGE', 'C:/OSPanel/domains/retailcrm/catalog/language/');
+define('DIR_TEMPLATE', 'C:/OSPanel/domains/retailcrm/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/OSPanel/domains/retailcrm/system/config/');
+define('DIR_CACHE', 'C:/OSPanel/domains/retailcrm/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OSPanel/domains/retailcrm/system/storage/download/');
+define('DIR_LOGS', 'C:/OSPanel/domains/retailcrm/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OSPanel/domains/retailcrm/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OSPanel/domains/retailcrm/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'db_retailcrm');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
